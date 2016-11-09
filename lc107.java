08145f3cfe42464300d6c5c35a94b00e3f0154bf
lin <linhangyouxiang@outlook.com>
2016-11-08 19:15:37 -0500

    11082016

--- lc107.java
+++ lc107.java
@@ -9,30 +9,23 @@
  */
 public class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        if(root==null){
-            return list;
-        }
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        while (queue.size()!=0){
-            List<Integer> alist = new ArrayList<Integer>();
-            for (TreeNode child : queue){
-                alist.add(child.val);
-                
-            }
-            list.add(new ArrayList<Integer>(alist));
-            Queue<TreeNode> queue2 = queue;
-            queue = new LinkedList<TreeNode>();
-            for(TreeNode child:queue2){
-                if(child.left!=null)
-                    queue.add(child.left);
-                if(child.right!=null)
-                    queue.add(child.right);
-            }
-        
-        }
-        return list;
-        
+        List<List<Integer>> traversal = new ArrayList<List<Integer>>();  
+          
+        if(root == null) return traversal;  
+          
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();  
+        queue.add(root);  
+        while(!queue.isEmpty()) {  
+            int levelLen = queue.size();  
+            List<Integer> levelNodes = new ArrayList<Integer>();              
+            for (int i = 0; i < levelLen; i++) {  
+                TreeNode node = queue.removeFirst();  
+                levelNodes.add(node.val);  
+                if(node.left != null) queue.add(node.left);  
+                if(node.right != null) queue.add(node.right);  
+            }  
+            traversal.add(0, levelNodes);    
+        }  
+        return traversal;  
     }
 }
