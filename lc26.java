cae461ee346097584d9854cfb8a6986e295e058e
lin <linhangyouxiang@outlook.com>
2016-12-03 15:31:31 -0500

    Dec.3

---
+++ lc26.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        if(nums.length==1){
+            
+            return 1;
+        }
+        
+        int index = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=nums[index]){
+                index++;
+                nums[index]=nums[i];
+                
+            }
+            
+        }
+        return index+1;
+    }
+}
