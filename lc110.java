7d50a953fbec752334a3dccc980dca3c4f3c9252
lin <linhangyouxiang@outlook.com>
2016-12-18 14:00:31 -0500

    。

---
+++ lc110.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root==null) return true;
+        if (Math.abs(maxDepth(root.left)-maxDepth(root.right))>1) return false;
+        return isBalanced(root.left)&&isBalanced(root.right); 
+    }
+    
+    static public int maxDepth(TreeNode root){
+        
+        if (root==null) return 0;
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+        
+    }
+    
+}
